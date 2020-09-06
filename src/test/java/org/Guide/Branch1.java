ff0e93df3b8d6ec00f94f927233fc982dafadfb2
rajiktm20 <rajiktm2014@gmail.com>
2020-09-06 13:38:53 +0530

    initial commit

---
+++ src/test/java/org/Guide/Branch1.java
@@ -0,0 +1,13 @@
+package org.Guide;
+
+public class Branch1 {
+	public void first() {
+		System.out.println("branch 1");
+		
+	}
+
+	public void second() {
+		System.out.println("branch 2");
+
+}
+}
